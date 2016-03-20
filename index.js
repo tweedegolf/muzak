8f080d2bd4c1f38dea52939b58f9f50245991f01
Marlon Baeten <marlon@tweedegolf.com>
2016-03-20 11:42:40 +0100

    Added webserver

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var dazeus = require('dazeus');
+var server = require('./src/hook_server');
+
+// lets parse command line args
+var argv = dazeus.optimist().argv;
+dazeus.help(argv);
 var options = dazeus.optionsFromArgv(argv);
 
 var client = dazeus.connect(options, function () {
@@ -6,3 +11,5 @@ var client = dazeus.connect(options, function () {
         client.message(network, channel, message);
     });
 });
+
+server.listen(8080);
