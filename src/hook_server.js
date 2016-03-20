53be10efb1560f7d8a0cdc710d572ec6667cac20
Marlon Baeten <marlon@tweedegolf.com>
2016-03-20 15:04:00 +0100

    jira hook + test added

--- src/hook_server.js
+++ src/hook_server.js
@@ -6,27 +6,56 @@ import moment from 'moment';
 import _ from 'lodash';
 
 var app = express();
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+const ISSUE_CREATED = 'jira:issue_created';
+const ISSUE_UPDATED = 'jira:issue_updated';
+
+ // for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to MUZAK, please use IRC to interact.');
+    res.send('Welcome to MUZAK, please use IRC to interact.');
 });
 
-app.post('/hooks/commit', (req, res) => {
-  var data = {};
+app.post('/commit', (req, res) => {
+    var data = {};
 
-  if (_.isObject(req.body)) {
-    data = req.body;
-  } else {
-    data = JSON.parse(req.body.payload);
-  }
+    // github has a payload param - gitlab uses the body
+    if (req.body.object_kind) {
+        // gitlab payload JSON
+        data = req.body;
+    } else {
+        // github payload JSON
+        data = JSON.parse(req.body.payload);
+    }
 
-  _.forEach(data.commits, (commit) => {
-    console.log("Commit by:", commit.author.email);
-  });
+    _.forEach(data.commits, (commit) => {
+        console.log('Commit by:', commit.author.email);
+    });
 
-  res.send('hook received succesfully at ' + moment().format());
+    res.send('[' + moment().format('YYYY-MM-DD hh:mm') + '] hook processed');
 });
+
+app.post('/issue/:key', function (req, res) {
+    var data = req.body;
+
+    if (data.webhookEvent === ISSUE_CREATED) {
+        console.log('issue created by:', data.user.emailAddress);
+    } else if (data.webhookEvent === ISSUE_UPDATED)  {
+        var action = 'changed';
+        _.forEach(data.changelog.items, (item) => {
+            if (item.field === 'status') {
+                action = item.toString;
+            }
+        });
+        console.log('issue ' + action + ' by:', data.user.emailAddress);
+    }
+
+    res.send('[' + moment().format('YYYY-MM-DD hh:mm') + '] hook processed');
+});
+
 
 export default app;
---
+++ test/hooks.js
@@ -0,0 +1,88 @@
+var querystring = require('querystring');
+var request = require('request');
+
+var url = 'http://localhost:8080/';
+
+var github_commit = {
+    "after":"1481a2de7b2a7d02428ad93446ab166be7793fbb",
+    "before":"17c497ccc7cca9c2f735aa07e9e3813060ce9a6a",
+    "commits":[
+       {
+          "author":{
+             "email":"github@sjorsgielen.nl",
+          }
+       },
+       {
+          "author":{
+             "email":"marlon@tweedegolf.com"
+          }
+       }
+    ],
+    "repository":{
+       "name":"testing",
+       "url":"https://github.com/octokitty/testing"
+    }
+};
+
+var gitlab_commit = {
+  "object_kind": "push",
+  "before": "95790bf891e76fee5e1747ab589903a6a1f80f22",
+  "after": "da1560886d4f094c3e6c9ef40349f7d38b5d27d7",
+  "repository":{
+    "name": "Diaspora",
+    "url": "git@example.com:mike/diasporadiaspora.git"
+  },
+  "commits": [
+    {
+      "id": "b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327",
+      "author": {
+        "email": "marlon@tweedegolf.com"
+      }
+    },
+    {
+      "id": "da1560886d4f094c3e6c9ef40349f7d38b5d27d7",
+      "author": {
+        "email": "nick@astrant.net"
+      }
+    }
+  ]
+};
+
+var jira_hook =  {
+  "issue": {
+    "key":"JRA-20002",
+    "fields":{
+      "summary":"Implemented Musac",
+    }
+  },
+  "user": {
+    "emailAddress":"github@sjorsgielen.nl",
+  },
+  "changelog": {
+    "items": [
+      {
+        "toString": "resolved",
+        "field": "status"
+      }
+    ]
+  },
+  "webhookEvent": "jira:issue_updated"
+};
+
+console.log('Testing github hook:', url);
+
+var payload = JSON.stringify(github_commit);
+
+request.post({url: url + 'commit', form: {payload: payload}}, function(err, res, body) {
+    console.log('Received:', body);
+});
+
+payload = JSON.stringify(gitlab_commit);
+
+request.post({url: url + 'commit', form: gitlab_commit}, function(err, res, body) {
+    console.log('Received:', body);
+});
+
+request.post({url: url + 'issue/MUS-1', form: jira_hook}, function(err, res, body) {
+    console.log('Received:', body);
+});
